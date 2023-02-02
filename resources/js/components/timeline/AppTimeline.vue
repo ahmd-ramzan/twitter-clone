726b35f93d2fec557d8f1e178ceb257a9560b94d
ahmd-ramzan <iamahmdrmzn@gmail.com>
2023-02-02 07:55:21 +0500

    rendering tweets on timeline

---
+++ resources/js/components/timeline/AppTimeline.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <app-tweet
+            v-for="tweet in tweets"
+            :key="tweet.id"
+            :tweet="tweet"
+        />
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    name: "AppTimeline",
+    data() {
+        return {
+            tweets: []
+        }
+    },
+    methods: {
+        async getTweets() {
+            let response = await axios.get('/api/timeline')
+            this.tweets = response.data.data
+        }
+    },
+    created() {
+        this.getTweets()
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
