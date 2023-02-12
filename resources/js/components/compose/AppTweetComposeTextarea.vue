b0b3ee0124fe4fb05aa6cc166b7b53e5af613018
ahmd-ramzan <iamahmdrmzn@gmail.com>
2023-02-12 14:13:06 +0500

    adjusting textarea

---
+++ resources/js/components/compose/AppTweetComposeTextarea.vue
@@ -0,0 +1,22 @@
+<template>
+    <textarea
+        class="bg-gray-900 w-full outline-none text-gray-300 text-lg resize-none mb-2"
+        placeholder="What's happening"
+        autofocus
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value); resize($event)"
+    >
+    </textarea>
+</template>
+
+<script>
+export default {
+    props: ['modelValue'],
+    methods: {
+        resize(e) {
+            e.target.style.height = 'auto'
+            e.target.style.height = `${e.target.scrollHeight}px`
+        }
+    }
+}
+</script>
